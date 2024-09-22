a69e520c0bb9c7a555be95f375188b861ff04bc9
mementomorinn <qweasejlvg@gmail.com>
2024-09-22 14:21:28 +0700

    rules

--- modal.js
+++ modal.js
@@ -2,25 +2,19 @@
 const modalRules = document.getElementById("modal-rules");
 const openRules = document.getElementById("open-modal-rules");
 const closeRules = document.getElementById("close-modal-rules");
-
 openRules.addEventListener("click", function () {
-  modalRules.style.display = "block";
-  modalRules.style.transition = "0.3s";
+  modalRules.classList.add("open");
 });
-
 closeRules.addEventListener("click", function () {
-  modalRules.style.display = "none";
+  modalRules.classList.remove("open");
 });
-
 //settings
 const modalSett = document.getElementById("modal-settings");
 const openSett = document.getElementById("open-modal-settings");
 const closeSett = document.getElementById("close-modal-settings");
-
 openSett.addEventListener("click", function () {
   modalSett.style.display = "block";
 });
-
 closeSett.addEventListener("click", function () {
   modalSett.style.display = "none";
 });
