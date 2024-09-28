9aed745b04f4390d1a084d8da86d11ed28808a55
mementomorinn <qweasejlvg@gmail.com>
2024-09-28 19:25:04 +0700

    new OnOff

--- js/score.js
+++ js/score.js
@@ -1,37 +1,38 @@
 //setting
 const btnTimer = document.getElementById("timer");
 const btnCheckCell = document.getElementById("check-cell");
+const settTimer = btnTimer.parentElement;
+const settCheck = btnCheckCell.parentElement;
 let isTimer = 1;
 let isCheckCell = 0;
 
 //включение кнопки
-function turnOn(btn) {
-  const parent = btn.parentElement;
-  parent.classList.add("on");
-}
-//выключение кнопки
-function turnOff(btn) {
-  const parent = btn.parentElement;
-  parent.classList.remove("on");
-}
+// function turnOn(btn) {
+//   settTimer.classList.add("on");
+// }
+// //выключение кнопки
+// function turnOff(btn) {
+//   settCheck.classList.remove("on");
+// }
 //тык на кнопку
 btnTimer.addEventListener("click", function (e) {
   if (isTimer) {
-    turnOff(e.target);
+    settTimer.classList.remove("on");
     isTimer = 0;
   } else {
-    turnOn(e.target);
+    settTimer.classList.add("on");
     isTimer = 1;
   }
+  console.log(isTimer);
 });
 
 btnCheckCell.addEventListener("click", function (e) {
   if (isCheckCell) {
-    turnOff(e.target);
+    settCheck.classList.remove("on");
     isCheckCell = 0;
     console.log(isCheckCell);
   } else {
-    turnOn(e.target);
+    settCheck.classList.add("on");
     isCheckCell = 1;
     console.log(isCheckCell);
   }
